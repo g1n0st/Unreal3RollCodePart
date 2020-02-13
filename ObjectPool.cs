98987e2603c7a78d948f01b5e06e044b36b0728b
g1n0st <g1n0st@live.com>
2020-02-13 23:00:04 +0800

    Create ObjectPool.cs

---
+++ ObjectPool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+	public static ObjectPool SharedInstancePool;
+
+	public GameObject objectToPool;
+	public int amountToPool;
+
+	List<GameObject> pooledObjects;
+
+	void Start()
+	{
+		SharedInstancePool = this;
+
+		pooledObjects = new List<GameObject>();
+		for (int i = 0; i < amountToPool; i++)
+		{
+			GameObject obj = (GameObject)Instantiate(objectToPool);
+			obj.SetActive(false);
+			pooledObjects.Add(obj);
+		}
+
+	}
+
+	public GameObject GetPooledObject()
+	{
+		for (int i = 0; i < pooledObjects.Count / 20; i++)
+		{
+			int k = Random.Range(0, pooledObjects.Count);
+			if (!pooledObjects[k].activeInHierarchy)
+			{
+				return pooledObjects[k];
+			}
+		}
+
+		GameObject obj = (GameObject)Instantiate(objectToPool);
+		obj.SetActive(false);
+		pooledObjects.Add(obj);
+
+		return null;
+	}
+
+	void Update()
+	{
+
+	}
+}
